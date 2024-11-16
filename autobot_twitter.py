6061d50156dd633ad4c272a7da7e2c9162bbe70c
bot SAM <128407982+TAHKInteractingAI@users.noreply.github.com>
2024-11-16 09:10:23 +0700

    Update autobot_twitter.py

--- autobot_twitter.py
+++ autobot_twitter.py
@@ -217,7 +217,7 @@ try:
     for index, row in df.loc[df['Status'] == ''].iterrows():
         post_tweet_from_row(row, index)
 except
-    print('No empty cells were found in Status column")
+    print("No empty cells were found in Status column")
 
 # Update Google Sheets with the new status
 def update_google_sheet_status(df, spreadsheet_id, range_name):
